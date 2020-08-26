2bad3b80b84740f400388f28ceac732a798f9138
Mazhar Khan <66962188+khanmazhar@users.noreply.github.com>
2020-08-25 20:04:49 -0700

    updated the code

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,20 @@
-function palindrome(str) {
-  const regex = /[\W_]/g;
-  var alphaNumeric = str.replace(regex, "").toLowerCase();
-  var reversed = alphaNumeric.split('').reverse().join('');
-  //console.log(alphaNumeric)
-  if (reversed === alphaNumeric) {
-    return true;
-  } else {
-    return false;
-  }
+displayNone();
 
+document.getElementById('btn').addEventListener('click', function () {
+    var input = document.getElementById('text-input').value;
+    const regex = /[\W_]/g;
+    var orignal = input.replace(regex, "").toLowerCase();
+    var reversed = orignal.split('').reverse().join('');
+    if (orignal === reversed) {
+        document.querySelector('.correct').style.display = 'block';
+        document.querySelector('#text-input').addEventListener('keyup', displayNone);
+    } else {
+        document.querySelector('.wrong').style.display = 'block';
+        document.querySelector('#text-input').addEventListener('keyup', displayNone);
+    }
+})
+
+function displayNone() {
+    document.querySelector('.correct').style.display = 'none';
+    document.querySelector('.wrong').style.display = 'none';
 }
-
-
-
-palindrome("My age is 0, 0 si ega ym.");
